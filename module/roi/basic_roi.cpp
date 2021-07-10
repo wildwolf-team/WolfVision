506ee7d118413551c191a92f6e2b0e3fe04cfe30
PaPr <36095960+PaPaPR@users.noreply.github.com>
2021-07-10 16:08:43 +0800

    feat: init abstract_roi and basic_roi, misc changes

--- module/roi/basic_roi.cpp
+++ module/roi/basic_roi.cpp
@@ -1,5 +1,81 @@
 #include "basic_roi.hpp"
 
 namespace basic_roi {
+cv::Rect ImageRoi::makeRectSafe_Fixed(const cv::Mat &_input_img,
+                                      const cv::RotatedRect &_r_rect) {
+  int width = _r_rect.boundingRect().width;
+  int height = _r_rect.boundingRect().height;
 
+  cv::Point tl =
+      cv::Point(_r_rect.center.x - width * 0.5f, _r_rect.center.y * 0.5f);
+  if (tl.x < 0) {
+    tl.x = 0;
+  }
+  if (tl.y < 0) {
+    tl.y = 0;
+  }
+
+  if (tl.x + width > _input_img.cols) {
+    tl.x -= (tl.x + width - _input_img.cols);
+  }
+
+  if (tl.y + height > _input_img.rows) {
+    tl.y -= (tl.y + height - _input_img.rows);
+  }
+
+  return cv::Rect(tl.x, tl.y, width, height);
+} 
+
+cv::Rect ImageRoi::makeRectSafe_Tailor(const cv::Mat &_input_img,
+                                       const cv::RotatedRect &_r_rect) {
+  int width = _r_rect.boundingRect().width;
+  int height = _r_rect.boundingRect().height;
+
+  cv::Point tl =
+      cv::Point(_r_rect.center.x - width * 0.5f, _r_rect.center.y * 0.5f);
+
+  if (tl.x < 0) {
+    width -= (0 - tl.x);
+    tl.x = 0;
+  }
+  if (tl.y < 0) {
+    height -= (0 - tl.y);
+    tl.y = 0;
+  }
+
+  if (tl.x + width > _input_img.cols) {
+    width -= (tl.x + width - _input_img.cols);
+  }
+  if (tl.y + height > _input_img.rows) {
+    height -= (tl.y + height - _input_img.rows);
+  }
+
+  return cv::Rect(tl.x, tl.y, width, height);
+}
+
+cv::Rect ImageRoi::makeRectSafe_Tailor(const cv::Mat &_input_img,
+                                       const cv::Rect &_r_rect) {
+  int width = _r_rect.width;
+  int height = _r_rect.height;
+
+  cv::Point tl = _r_rect.tl();
+
+  if (tl.x < 0) {
+    width -= (0 - tl.x);
+    tl.x = 0;
+  }
+  if (tl.y < 0) {
+    height -= (0 - tl.y);
+    tl.y = 0;
+  }
+
+  if (tl.x + width > _input_img.cols) {
+    width -= (tl.x + width - _input_img.cols);
+  }
+  if (tl.y + height > _input_img.rows) {
+    height -= (tl.y + height - _input_img.rows);
+  }
+
+  return cv::Rect(tl.x, tl.y, width, height);
+}
 }
--- module/roi/basic_roi.hpp
+++ module/roi/basic_roi.hpp
@@ -3,5 +3,18 @@
 #include "abstract_roi.hpp"
 
 namespace basic_roi {
+class ImageRoi : public abstract_roi::Abstract_ImageRoi {
+ public:
+  ImageRoi() {}
+  ~ImageRoi() {}
 
-}
+  cv::Rect makeRectSafe_Fixed(const cv::Mat &_input_img,
+                              const cv::RotatedRect &_r_rect);
+
+  cv::Rect makeRectSafe_Tailor(const cv::Mat &_input_img,
+                               const cv::RotatedRect &_r_rect);
+
+  cv::Rect makeRectSafe_Tailor(const cv::Mat &_input_img,
+                               const cv::Rect &_r_rect);
+};
+}  // namespace basic_roi
