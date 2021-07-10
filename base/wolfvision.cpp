f6e5683fc36b06acb2bea7456776ce20f23f098d
iChizer0 <62390647+iChizer0@users.noreply.github.com>
2021-07-10 20:07:18 +0800

    feat: add CONFIG_FILE_PATH support, misc fixes

--- base/wolfvision.cpp
+++ base/wolfvision.cpp
@@ -2,6 +2,7 @@
 
 int main() {
     fmt::print("[wolfvision] WolfVision built on g++ version: {}\n", __VERSION__);
+    fmt::print("[wolfvision] WolfVision config file path: {}\n", CONFIG_FILE_PATH);
 
     return 0;
 }
