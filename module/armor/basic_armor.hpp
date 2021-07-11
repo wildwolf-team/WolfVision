faba00799d4b26561b24ade1d35a43695593ba30
iChizer0 <62390647+iChizer0@users.noreply.github.com>
2021-07-11 18:56:39 +0800

    chore: minify header includes, remove -lpthread flag

--- module/armor/basic_armor.hpp
+++ module/armor/basic_armor.hpp
@@ -7,7 +7,9 @@
 #include <fmt/core.h>
 #include <fmt/color.h>
 
-#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 
 #include "uart_serial.hpp"
 
