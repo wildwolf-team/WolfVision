5b9297d4327e2e277f36fcac785105fbb701bab6
fqjun <1767851382@qq.com>
2021-07-12 03:52:09 +0800

    feat: add center_r

---
+++ module/buff/abstract_center_r.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <vector>
+
+#include "abstract_object.hpp"
+
+namespace abstract_center_r {
+class Center_R : public abstract_object::Object {
+ public:
+  /**
+   * @brief 输入参数
+   * @param[in]  _contours        轮廓点集
+   * @param[in]  _roi_img         圆形区域ROI
+   */
+  void inputParams(const std::vector<cv::Point>& _contours, const cv::Mat& _roi_img);
+
+  /**
+   * @brief 返回目标距离ROI图像中心点的距离
+   * @return float
+   */
+  float centerDist();
+
+ private:
+  // 中心距离
+  float center_distance;
+};
+
+inline float Center_R::centerDist() { return this->center_distance; }
+
+void Center_R::inputParams(const std::vector<cv::Point>& _contours, const cv::Mat& _roi_img) {
+  abstract_object::Object::inputParams(_contours);
+
+  // 中心距离
+  this->center_distance = abstract_object::centerDistance(
+      this->rect_.center, cv::Point(_roi_img.cols * 0.5, _roi_img.rows * 0.5));
+}
+}  // namespace abstract_center_r
