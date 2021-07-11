b8fc8adad7a9aa85bf148918e2829da739bdfd86
fqjun <1767851382@qq.com>
2021-07-12 03:51:03 +0800

    feat: add fan armor

--- module/armor/fan_armor.cpp
+++ module/armor/fan_armor.cpp
@@ -1,5 +1,24 @@
 #include "fan_armor.hpp"
 
-namespace fan_armor {
+#include <vector>
 
+namespace fan_armor {
+FanArmor::FanArmor() {}
+
+FanArmor::~FanArmor() {}
+
+void FanArmor::inputParams(const std::vector<cv::Point>& _contours) {
+  abstract_object::Object::inputParams(_contours);
 }
+
+void FanArmor::displayFanArmor(cv::Mat& _img) {
+  cv::Scalar color = cv::Scalar(0, 255, 0);
+
+  cv::Point put_fan_armor = cv::Point(_img.cols - 100, 10);
+  cv::putText(_img, " Fan Armor ", put_fan_armor, cv::FONT_HERSHEY_PLAIN, 1, color, 1, 8, false);
+
+  for (int k = 0; k < 4; ++k) {
+    cv::line(_img, this->vertex_[k], this->vertex_[(k + 1) % 4], color, 2);
+  }
+}
+}  // namespace fan_armor
--- module/armor/fan_armor.hpp
+++ module/armor/fan_armor.hpp
@@ -1,7 +1,31 @@
 #pragma once
 
+#include <vector>
+
 #include "abstract_armor.hpp"
+#include "module/buff/abstract_object.hpp"
 
 namespace fan_armor {
+class FanArmor : public abstract_object::Object {
+ public:
+  FanArmor();
+  ~FanArmor();
 
-}
+  /**
+   * @brief 输入参数
+   * @param[in]  _contours        输入轮廓点集
+   */
+  void inputParams(const std::vector<cv::Point>& _contours);
+
+  /**
+   * @brief 显示内轮廓
+   * @param[out]  _img     输出图像
+   * @note 绿色
+   * @note 图例显示在右侧
+   */
+  void displayFanArmor(cv::Mat& _img);
+
+ private:
+  /* anything else */
+};
+}  // namespace fan_armor
