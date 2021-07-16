41c96d6655e6bc6d9f6a038ea1eb0d5eaa73e958
fqjun <1767851382@qq.com>
2021-07-16 23:41:42 +0800

    feat: add comments

--- module/armor/fan_armor.hpp
+++ module/armor/fan_armor.hpp
@@ -12,8 +12,18 @@ class Detector : public abstract_object::Object {
 
   ~Detector() = default;
 
+  /**
+   * @brief 输入参数
+   * @param[in]  _contours        输入轮廓点集
+   */
   void inputParams(const std::vector<cv::Point>& _contours);
 
+  /**
+   * @brief 显示内轮廓
+   * @param[out]  _img     输出图像
+   * @note 绿色
+   * @note 图例显示在右侧
+   */
   void displayFanArmor(cv::Mat& _img);
 };
 
