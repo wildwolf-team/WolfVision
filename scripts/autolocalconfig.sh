c24a1a1a76e62619bba58ab4e70e69e8c0f982aa
fqjun <1767851382@qq.com>
2021-07-21 02:32:12 +0800

    feat: add local autoconfing bash file

--- scripts/autolocalconfig.sh
+++ scripts/autolocalconfig.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Remove build folder
+FOLDER=build
+if  [ ! -d  "$FOLDER"  ]; then
+echo "Folder does not exist, please return to the root directory to execute "
+else
+echo "Folder exist"
+sudo rm -rf $FOLDER
+fi
+
 # Get git submodules
 git submodule update --init --recursive --depth 1
 
