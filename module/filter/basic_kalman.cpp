669becff7cd3d1203ab736423cf2248bd77b6f92
suhuolin <>
2021-07-21 21:11:51 +0800

    更新二阶预测

--- module/filter/basic_kalman.cpp
+++ module/filter/basic_kalman.cpp
@@ -2,4 +2,56 @@
 
 namespace basic_kalman {
 
+RM_kalmanfilter::RM_kalmanfilter() : KF_(4, 2) {
+  measurement_matrix   = Mat::zeros(2, 1, CV_32F);
+  KF_.transitionMatrix = (Mat_<float>(4, 4) << 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);
+
+  setIdentity(KF_.measurementMatrix, Scalar::all(1));
+  setIdentity(KF_.processNoiseCov, Scalar::all(1e-3));
+  setIdentity(KF_.measurementNoiseCov, Scalar::all(1e-2));  // 测量协方差矩阵R，数值越大回归越慢
+  setIdentity(KF_.errorCovPost, Scalar::all(1));
+
+  KF_.statePost = (Mat_<float>(4, 1) << x, y, 0, 0);        // 初始值
+}
+
+RM_kalmanfilter::~RM_kalmanfilter() {}
+
+Point2f RM_kalmanfilter::predict_point(Point2f _p) {
+  // p = _p;
+  Mat     prediction = KF_.predict();
+  Point2f predict_pt = Point2f(prediction.at<float>(0), 0);
+
+  measurement_matrix.at<float>(0, 0) = _p.x;
+
+  KF_.correct(measurement_matrix);
+  return predict_pt;
+}
+
+void RM_kalmanfilter::reset() { measurement_matrix = Mat::zeros(2, 1, CV_32F); }
+
+//
+float RM_kalmanfilter::use_RM_KF(float top) {
+  top_angle_differ->top_angle_ = top;
+
+  // 第一次获取的陀螺仪数据时, 对上一时刻(不存在)的陀螺仪数据的假设
+  if (top_angle_differ->get_top_times == 0) {
+    top_angle_differ->top_angle = 0;
+  }
+
+  // if(top_angle_differ->get_top_times % 1 == 0)
+  // {
+  std::cout << "top: " << top << std::endl;
+
+  top_angle_differ->get_top_times++;  // 自动计数 -> 获得陀螺仪数据的次数
+
+  top_angle_differ->differ = top_angle_differ->top_angle_ - top_angle_differ->top_angle;
+
+  top_angle_differ->top_angle = top_angle_differ->top_angle_;  // 这一时刻的陀螺仪数据 更新为 上一时刻的陀螺仪数据
+
+  std::cout << "top_angle_differ " << top_angle_differ->differ << std::endl;
+  // waiting shl...
+  // how to using the differ param....
+  Point2f top_differ = Point2f(top_angle_differ->differ * 10, 0);
+  return predict_point(top_differ).x;  // top_angle_differ->differ
+                                       // }
 }
--- module/filter/basic_kalman.hpp
+++ module/filter/basic_kalman.hpp
@@ -1,54 +1,99 @@
 #pragma once
 
-#include "abstract_kalman.hpp"
+
 
 namespace basic_kalman {
-    class Kalman1
-{
-public:
-    Kalman1(){
+
+
+#ifndef RM_KALMANFILTER_H
+#  define RM_KALMANFILTER_H
+
+#  include <iostream>
+using namespace std;
+#  include <opencv2/opencv.hpp>
+#  include <vector>
+using namespace cv;
+#  include <queue>
+
+
+class RM_kalmanfilter {
+ private:
+  cv::KalmanFilter KF_;
+  Mat              measurement_matrix;
+
+  float p;
+  float x = 640;
+  float y = 320;
+
+  typedef struct {
+    const int         QUEUE_NUM = 5 + 1;  // 队列大小+1
+    float             top_angle;          // 上一刻陀螺仪数据
+    float             top_angle_;         // 当前的陀螺仪数据
+    float             differ;             // 前后时刻的差值
+    int               get_top_times = 0;  // 陀螺仪数据获取次数
+    std::queue<float> difference_queue;   // difference_队列
+  } top_diff_Queue;
+  top_diff_Queue  temp;
+  top_diff_Queue* top_angle_differ = &temp;
+
+ public:
+  RM_kalmanfilter();
+  ~RM_kalmanfilter();
+  Point2f predict_point(Point2f _p);
+  void    reset();
+
+  float use_RM_KF(float top);
+  // void use_RM_kf(top_diff_Queue *top_angle_differ, serial_port::SerialPort serial_);
+
+  float anti_range = 1.5;
+};
+
+#endif
+class Kalman1 {
+ public:
+    Kalman1() {
         Q_ = 0.01f;
         R_ = 0.02f;
         t_ = 1.0f;
         x_ = 0.0f;
         p_ = 0.01f;
     }
-    Kalman1(float Q, float R, float t, float x0, float p0){
+    Kalman1(float Q, float R, float t, float x0, float p0) {
         Q_ = Q;
         R_ = R;
         t_ = t;
         x_ = x0;
         p_ = p0;
     }
-    void setParam(int R, int Q, int t){
-        if(R<1)
-            R=1;
-        if(Q<1)
-            Q=1;
-        if(t<1)
-            t=1;
+    void setParam(int R, int Q, int t) {
+        if (R < 1)
+            R = 1;
+        if (Q < 1)
+            Q = 1;
+        if (t < 1)
+            t = 1;
         R_ = static_cast<float>(R*0.01f);
         Q_ = static_cast<float>(Q*0.01f);
         t_ = static_cast<float>(t);
     }
-    float run(float data){
-       x_pre_ = x_;                                      //x(k|k-1) = AX(k-1|k-1)+BU(k)
-        p_pre_ = p_ + Q_;                              //p(k|k-1) = Ap(k-1|k-1)A'+Q
-        kg_ = p_pre_ / (p_pre_ + R_);               //kg(k) = p(k|k-1)H'/(Hp(k|k-1)'+R)
-        x_ = x_pre_ + kg_ * (data - x_pre_);          //x(k|k) = X(k|k-1)+kg(k)(Z(k)-HX(k|k-1))
-        p_ = (1 - kg_) * p_pre_;                   //p(k|k) = (I-kg(k)H)P(k|k-1)
+    float run(float data) {
+       x_pre_ = x_;                                   // x(k|k-1) = AX(k-1|k-1)+BU(k)
+        p_pre_ = p_ + Q_;                             // p(k|k-1) = Ap(k-1|k-1)A'+Q
+        kg_ = p_pre_ / (p_pre_ + R_);                 // kg(k)    = p(k|k-1)H'/(Hp(k|k-1)'+R)
+        x_ = x_pre_ + kg_ * (data - x_pre_);          // x(k|k)   = X(k|k-1)+kg(k)(Z(k)-HX(k|k-1))
+        p_ = (1 - kg_) * p_pre_;                      // p(k|k)   = (I-kg(k)H)P(k|k-1)
         return x_;
     }
-    float merge_run(float data1, float data2)
-    {
+    float merge_run(float data1, float data2) {
         x_pre_ = data1;
-        p_pre_ = p_ + Q_;                              //p(k|k-1) = Ap(k-1|k-1)A'+Q
-        kg_ = p_pre_ / (p_pre_ + R_);               //kg(k) = p(k|k-1)H'/(Hp(k|k-1)'+R)
-        x_ = x_pre_ + kg_ * (data2 - x_pre_);          //x(k|k) = X(k|k-1)+kg(k)(Z(k)-HX(k|k-1))
-        p_ = (1 - kg_) * p_pre_;                   //p(k|k) = (I-kg(k)H)P(k|k-1)
+        p_pre_ = p_ + Q_;                             // p(k|k-1) = Ap(k-1|k-1)A'+Q
+        kg_ = p_pre_ / (p_pre_ + R_);                 // kg(k)    = p(k|k-1)H'/(Hp(k|k-1)'+R)
+        x_ = x_pre_ + kg_ * (data2 - x_pre_);         // x(k|k)   = X(k|k-1)+kg(k)(Z(k)-HX(k|k-1))
+        p_ = (1 - kg_) * p_pre_;                      // p(k|k)   = (I-kg(k)H)P(k|k-1)
         return x_;
     }
-public:
+
+ public:
     float R_;
     float Q_;
     float p_pre_;
@@ -57,7 +102,6 @@ public:
     float p_;
     float kg_;
     float t_;
-
 };
 
-}
+}  // namespace basic_kalman
