76365c7a1ee1848577a5cc69f4a8eb320d0f14a9
gcusms <2436210442@qq.com>
2021-08-31 13:22:11 +0800

    fix:change the wolfvision.cpp

--- base/wolfvision.cpp
+++ base/wolfvision.cpp
@@ -41,7 +41,7 @@ int main() {
   cv::VideoWriter vw_src;
   cv::FileStorage re_config_get(record_.video_save_path_, cv::FileStorage::READ);
   re_config_get["_PATH"] >> record_.path_;
-  vw_src.open(CONFIG_FILE_PATH + '/record/' + to_string(record_.path_) + '.avi', cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 66, cv::Size(1280, 800), true);
+  vw_src.open(CONFIG_FILE_PATH + '/record/' + record_.path_+ '.avi', cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 66, cv::Size(1280, 800), true);
   basic_roi::RoI save_roi;
   fps::FPS       global_fps_;
 
